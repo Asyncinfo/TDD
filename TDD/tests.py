68c39144a5d0d1da0ced5ba8d876f6c8549120b9
Asyncinfo <setsuna0728@hotmail.com>
2020-08-15 22:06:53 +0800

    template used

--- TDD/tests.py
+++ TDD/tests.py
@@ -11,12 +11,31 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page(self):
+    def test_home_page1(self):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        """ self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>')) """
+        self.assertTrue(html.endswith('</html>'))
+        
+    def test_home_page2(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
         expected_html = render_to_string('home.html')
         self.assertEqual(html, expected_html)
+
+    def test_home_page3(self):
+        response = self.client.get('/')
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page4(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
