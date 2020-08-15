38d05c7315ccb99949b0c2c267bc60e372b5327b
Asyncinfo <setsuna0728@hotmail.com>
2020-08-15 13:42:52 +0800

    functional_tests

--- TDD/views.py
+++ TDD/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-home_page = None
+from django.http import HttpResponse
+
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
