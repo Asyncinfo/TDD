629cf8cf543eaa81756980e91ffd2444e447629c
Asyncinfo <setsuna0728@hotmail.com>
2020-08-17 20:08:59 +0800

    update

--- TDD/tests.py
+++ TDD/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from TDD.views import home_page
+from TDD.models import Item
 
 class HomePageTest(TestCase):
 
@@ -11,7 +12,7 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page1(self):
+    """ def test_home_page1(self):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
@@ -34,8 +35,34 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'home.html') """
 
     def test_home_page4(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_times = Item.objects.all()
+        self.assertEqual(saved_times.count(), 2)
+
+        first_saved_item = saved_times[0]
+        second_saved_item = saved_times[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
